7f2fdb92f90b1814ca139074dbdf4a1fef7af77d
Guilherme Mendonca <guigmen@guigmen.com>
2021-11-11 15:57:13 -0300

    fix(flag): update styles from emotion to material

--- src/components/basic/Flag/Flag.styles.ts
+++ src/components/basic/Flag/Flag.styles.ts
@@ -1,19 +1,19 @@
-import styled from '@emotion/styled'
+import { styled } from '@mui/material/styles'
 
-export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 24px;
-  width: 24px;
-`
+export const Container = styled('div')(() => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: 24,
+  width: 24,
+}))
 
-export const FlagWrapper = styled.div`
-  height: 18px;
-  width: 24px;
-  overflow: hidden;
-  border-radius: 4px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
+export const FlagWrapper = styled('div')(() => ({
+  height: 18,
+  width: 24,
+  overflow: 'hidden',
+  borderRadius: '4px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}))
